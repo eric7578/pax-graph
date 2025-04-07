ef47ab51315707aeb6b863249db4fe2a8b401a7d
eric.tsai <eric7578@gmail.com>
2025-04-07 17:38:40 +0800

    calculate by time range

--- packages/crawler/src/index.ts
+++ packages/crawler/src/index.ts
@@ -1,5 +1,15 @@
 import axios from 'axios';
 import _ from 'lodash';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault('Asia/Taipei');
+
+const startAt = dayjs().startOf('hour');
+const range = [startAt.subtract(3, 'hours'), startAt];
 
 type Pax = {
   count: number;
@@ -29,7 +39,7 @@ async function getPaxData(url: string) {
 }
 
 async function main() {
-  const url = [
+  const leave = [
     'https://opendata.immigration.gov.tw/APIS/CYI5',
     'https://opendata.immigration.gov.tw/APIS/HUN5',
     'https://opendata.immigration.gov.tw/APIS/KHH5',
@@ -41,15 +51,36 @@ async function main() {
     'https://opendata.immigration.gov.tw/APIS/TSA5',
     'https://opendata.immigration.gov.tw/APIS/TTT5',
   ];
+  const enter = [
+    'https://opendata.immigration.gov.tw/APIS/CYI1',
+    'https://opendata.immigration.gov.tw/APIS/HUN1',
+    'https://opendata.immigration.gov.tw/APIS/KHH1',
+    'https://opendata.immigration.gov.tw/APIS/MZG1',
+    'https://opendata.immigration.gov.tw/APIS/PIF1',
+    'https://opendata.immigration.gov.tw/APIS/RMQ1',
+    'https://opendata.immigration.gov.tw/APIS/TNN1',
+    'https://opendata.immigration.gov.tw/APIS/TPE1',
+    'https://opendata.immigration.gov.tw/APIS/TSA1',
+    'https://opendata.immigration.gov.tw/APIS/TTT1',
+  ];
 
-  const paxRows = _.flatten(await Promise.all(url.map((u) => getPaxData(u))));
-  const groupByNatAge = _.groupBy(paxRows, (r) => r.nationality + r.age);
-  const rows: Pax[] = Object.values(groupByNatAge).map((r: Pax[]) => ({
-    nationality: r[0].nationality,
-    age: r[0].age,
-    count: _.sumBy(r, 'count'),
-  }));
-  console.log(rows);
+  const leaveRecords = _.flatten(
+    await Promise.all(leave.map((u) => getPaxData(u)))
+  );
+  const enterRecords = _.flatten(
+    await Promise.all(enter.map((u) => getPaxData(u)))
+  );
+  console.log({
+    range: range.map((r) => r.toDate().toISOString()),
+    leavePaxCount: leaveRecords.reduce(
+      (acc: number, r: Pax) => acc + r.count,
+      0
+    ),
+    enterPaxCount: enterRecords.reduce(
+      (acc: number, r: Pax) => acc + r.count,
+      0
+    ),
+  });
 }
 
 main();
